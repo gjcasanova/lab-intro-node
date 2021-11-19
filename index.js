2122190422a56058da2e8514d840d20c5961680c
Guillermo Casanova <guillermo_m_casanova@hotmail.com>
2021-11-19 14:37:19 -0500

    Some iterations solved

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length++;
+    if (this.length - 1 == 0) {
+      this.items.push(item);
+      return;
+    }
 
-  get(pos) {}
+    let pos = 0;
 
-  max() {}
+    while (this.items[pos] < item ) {
+      pos++;
+    }
 
-  min() {}
+    this.items.splice(pos, 0, item);
+  }
 
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length == 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
+
+  min() {
+    if (this.length == 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
+
+  // Bonus
   sum() {}
 
   avg() {}
